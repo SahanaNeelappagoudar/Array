481bba97fca6d4bebc8877b12549c1993f6901c6
Sahana <sahananeelppagoudar@gmail.com>
2025-11-21 06:24:40 +0530

    ABC branch array of minimum and maximum

--- Array.py
+++ Array.py
@@ -11,10 +11,16 @@ def main():
         
         total = sum(numbers)
         average = total / len(numbers)
+        maximum = max(numbers)
+        minimum = min(numbers)
+
 
         print("Scores:", numbers)
         print("Sum:", total)
         print("Average:", average)
+        print("Maximum:", maximum)
+        print("Minimum:", minimum)
+
 
     except ValueError:
         print("Error: Please enter only numbers separated by spaces")
